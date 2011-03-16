fc3e4b5a22e327d76b45aece95b0849b1b3bb62f
unknown <asmerdov@.corp.capgemini.com>
2011-03-16 18:28:44 +0100

    model added + xml serialization

---
+++ CapgeminiSurface/Model/Project.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapgeminiSurface.Model
+{
+    public class Project
+    {
+        string name;
+        string description;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public string Description
+        {
+            set { description = value; }
+            get { return description; }
+        }
+    }
+}
---
+++ CapgeminiSurface/Model/VisitCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CapgeminiSurface.Model
+{
+    public class VisitCard
+    {
+        string name;
+        string email;
+        string tlf;
+        string pictureFileName;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+        public string Email
+        {
+            set { email = value; }
+            get { return email; }
+        }
+        public string Tlf
+        {
+            set { tlf = value; }
+            get { return tlf; }
+        }
+        public string PictureFileName
+        {
+            set { pictureFileName = value; }
+            get { return pictureFileName; }
+        }
+    }
+}
